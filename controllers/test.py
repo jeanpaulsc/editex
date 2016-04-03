f607b7b449f8e51c5f35f0cee71b3eb925df93f0
jean-paul mccoy <jeanpaulsc@gmail.com>
2016-04-03 03:36:41 -0700

    database related record references

--- controllers/test.py
+++ controllers/test.py
@@ -12,3 +12,18 @@ def ins():
 def upd():
     rid=db.testes.update(2,sort=3)
     return dict(rid=rid)
+
+def upim():
+    stream = open("http://latex.codecogs.com/gif.latex?if%5C%3A%20%5Cexists%20%5C%3A",'rb') #open(filename, 'rb')
+    db.myfile.insert(image=stream)
+    rows = db(db.images.id>0).select()
+    return dict(rows=rows)
+
+def si():
+    import urllib
+    opener = urllib.FancyURLopener({})
+    f = opener.open("http://latex.codecogs.com/gif.latex?if%5C%3A%20%5Cexists%20%5C%3A")
+    stream = f.read()
+    db.images.insert(image=stream)
+    rows = db(db.images.id>0).select()
+    return dict(rows=rows)
